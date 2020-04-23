eeed41420d87ef0ab0820d441d8f816ae829d0eb
Chris <stephens-c2@live.com>
2020-04-22 17:38:22 -0700

    working off of previous project was able to tweak

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require ("./routes/apiRoutes")(app);
-require ("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONDOGDB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true
 });
 
 app.listen(PORT, function() {
-    console.log('now listening on port: ${PORT}');
+  console.log(`Now listening on port: ${PORT}`);
 });
